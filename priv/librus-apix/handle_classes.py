dec9048c8ea139b19580b8645b8a16802f40e625
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-11-29 23:04:08 +0100

    :snake: Add function for handling token class

--- priv/librus-apix/handle_classes.py
+++ priv/librus-apix/handle_classes.py
@@ -1,5 +1,9 @@
 from librus_apix.get_token import get_token
-
+from erlport.erlterms import Atom
 def handle_token(u, p):
-    token = get_token(u,p)
-    return token.__dict__
+    try:
+        token = get_token(u,p)
+        return Atom("ok".encode("utf-8")), token.API_Key
+    except Exception as ex:
+        return Atom("error".encode("utf-8")), str(ex)
+
