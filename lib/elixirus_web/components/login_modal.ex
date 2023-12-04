efa2004c69e95a5e7471afcd3e383e11564b6241
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-12-04 09:54:09 +0100

    :sparkles: Add a login form and login modal components

---
+++ lib/elixirus_web/components/login_modal.ex
@@ -0,0 +1,21 @@
+defmodule ElixirusWeb.LoginModal do
+  alias ElixirusWeb.LoginForm
+  use ElixirusWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div class={!@login_required && "hidden"}>
+      <div class=" z-50 ">
+        <div class=" fixed bg-blue-500/10  inset-0 transition-opacity" aria-hidden="true" />
+        <div class="fixed inset-0 overflow-y-auto" role="dialog" aria-modal="true" tabindex="0">
+          <div class="flex min-h-full items-center justify-center">
+            <div class="w-full flex items-center justify-center max-w-3xl p-4 sm:p-6 lg:py-8">
+              <.live_component module={LoginForm} id="login_form" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+end
