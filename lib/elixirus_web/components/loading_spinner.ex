9ca15640e0a6281a9ee22c6693114f680e0950e6
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-12-18 13:26:31 +0100

    :lipstick: Make it BIGGER

--- lib/elixirus_web/components/loading_spinner.ex
+++ lib/elixirus_web/components/loading_spinner.ex
@@ -5,7 +5,7 @@ defmodule ElixirusWeb.Components.Loadings do
     ~H"""
     <svg
       aria-hidden="true"
-      class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      class="w-24 h-24 text-gray-200 duration-1000 animate-spin dark:text-gray-600 fill-blue-600"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
