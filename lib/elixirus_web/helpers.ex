f4c8192fcf8f795c26a2a3f185ec99f4519eb7f6
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-12-18 12:12:34 +0100

    :bricks: move functions from timetable to helpers

--- lib/elixirus_web/helpers.ex
+++ lib/elixirus_web/helpers.ex
@@ -2,4 +2,13 @@ defmodule ElixirusWeb.Helpers do
   def stringify_value(data, charlist_key) do
     data |> Map.get(charlist_key) |> to_string()
   end
+
+  def warsaw_now() do
+    timezone = "Europe/Warsaw"
+    DateTime.now!(timezone)
+  end
+
+  def this_weeks_monday(date \\ warsaw_now()) do
+    date |> Date.beginning_of_week()
+  end
 end
