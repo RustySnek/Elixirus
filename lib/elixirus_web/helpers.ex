e078fbe7fa6be0d1699c9f36989cec6c878f8a6b
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-02-12 13:11:10 +0100

    :sparkles: Add handler for caching data

--- lib/elixirus_web/helpers.ex
+++ lib/elixirus_web/helpers.ex
@@ -11,4 +11,21 @@ defmodule ElixirusWeb.Helpers do
   def this_weeks_monday(date \\ warsaw_now()) do
     date |> Date.beginning_of_week()
   end
+
+  def handle_cache_data(user_id, cache_type) do
+    case Cachex.ttl(:elixirus_cache, user_id <> cache_type) do
+      {:ok, nil} ->
+        :load
+
+      {:ok, _} ->
+        case Cachex.get(:elixirus_cache, user_id <> cache_type) do
+          {:ok, nil} -> :load
+          {:ok, data} -> data
+          _ -> :load
+        end
+
+      _ ->
+        :load
+    end
+  end
 end
