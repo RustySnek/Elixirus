85bc389e2a6788d5c05e1a45dd7db4b46857cd7e
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-03-28 19:58:05 +0100

    :technologist: add an easier way to handle login

---
+++ lib/elixirus_web/login_handler.ex
@@ -0,0 +1,25 @@
+defmodule ElixirusWeb.LoginHandler do
+  defmacro __using__(_opts) do
+    quote do
+      def handle_event(
+            "navigate_students",
+            %{
+              "return_url" => return_url,
+              "token" => token,
+              "user_id" => user_id,
+              "username" => username
+            } = _params,
+            socket
+          ) do
+        socket =
+          socket
+          |> assign(:username, username)
+          |> assign(:token, token)
+          |> assign(:user_id, user_id)
+          |> assign(:login_required, false)
+
+        {:noreply, redirect(socket, to: return_url)}
+      end
+    end
+  end
+end
