7dfaca32a4b4dbbd6ec27f9897cfe564a3610ec3
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-04-07 18:54:52 +0200

    :technologist: Add proxy

--- lib/elixirus/healthcheck/health_supervisor.ex
+++ lib/elixirus/healthcheck/health_supervisor.ex
@@ -1,6 +1,7 @@
 defmodule Elixirus.Healthcheck.HealthSupervisor do
   alias Elixirus.Healthcheck.HealthWorker
   alias Elixirus.Healthcheck.Services.LibrusConnection
+  alias Elixirus.Healthcheck.Services.ProxyAlive
   use Supervisor
 
   def start_link(init_arg) do
@@ -9,7 +10,8 @@ defmodule Elixirus.Healthcheck.HealthSupervisor do
 
   def init(_args) do
     children = [
-      Supervisor.child_spec({HealthWorker, [LibrusConnection, 10_000]}, id: LibrusConnection)
+      Supervisor.child_spec({HealthWorker, [LibrusConnection, 10_000]}, id: LibrusConnection),
+      Supervisor.child_spec({HealthWorker, [ProxyAlive, 10_000]}, id: ProxyAlive)
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
