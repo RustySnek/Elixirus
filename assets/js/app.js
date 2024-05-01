0225a1919f46817c93f47ae8923eae5acf5eb649
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-01 08:52:18 +0200

    :zap: Add helper hooks for sending messages

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,6 +30,26 @@ Hooks.set_local_storage = {
   }
 }
 
+Hooks.handle_textarea = {
+  mounted() {
+    this.el.addEventListener("keydown", (event) => {
+      if (event.key === "Tab") {
+      event.preventDefault()
+        var start = this.el.selectionStart;
+    var end = this.el.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.el.value = this.el.value.substring(0, start) +
+      "\t" + this.el.value.substring(end);
+
+    // put caret at right position again
+    this.el.selectionStart =
+      this.el.selectionEnd = start + 1;
+      }
+    })
+  }
+}
+
 Hooks.retrieve_local_storage = {
   mounted() {
     let name = this.el.getAttribute("name")
@@ -41,6 +61,29 @@ Hooks.retrieve_local_storage = {
     }else {
     this.pushEvent("retrieve_local_storage", {[name]: item})
     }
+  }
+}
+
+Hooks.recipients_form = {
+mounted() {
+    this.el.addEventListener("submit", (event) => {
+      event.preventDefault();
+      input = this.el.querySelector("#recipient-name-input")
+      name = input.value
+      input.value = ""
+    })
+  }
+}
+
+Hooks.form_confirmation = {
+  mounted() {
+    this.el.addEventListener("click", (e) => {
+      e.preventDefault();
+      let confirmation = confirm("Submit the form?")
+      if (confirmation) {
+        this.pushEvent("send_message")
+      }
+    })
   }
 }
 
