d1ab900067550cec0ebe86a7d12c58cb6b82f3b1
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-15 15:10:17 +0200

    :bug: try 30min intervals

--- lib/elixirus/token_worker.ex
+++ lib/elixirus/token_worker.ex
@@ -1,86 +1,84 @@
 defmodule Elixirus.TokenWorker do
-  use GenServer
-  alias Timex
-  import Elixirus.PythonWrapper
+use GenServer
+alias Timex
+import Elixirus.PythonWrapper
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
-  end
-
-  def init([table_name | opts]) do
-    table = :ets.new(table_name, opts)
-    {:ok, table, {:continue, :init_status}}
-  end
-
-  def handle_call({:add_token, username, token, ttl}, _from, table) do
-    :ets.insert(table, {username, token, ttl, DateTime.now!("Europe/Warsaw")})
-    {:reply, :ok, table}
-  end
-
-  def handle_call({:extend_lifetime, token}, _from, table) do
-    case :ets.match(table, {:"$1", token, :"$2", :_}) do
-      [] ->
-        :ok
-
-      [[name, ttl] | _] ->
-        unless(name == nil,
-          do: :ets.insert(table, {name, token, ttl, DateTime.now!("Europe/Warsaw")})
-        )
-    end
-
-    {:reply, :ok, table}
-  end
-
-  def handle_call({:extend_lifetime, token, ttl}, _from, table) do
-    case :ets.match(table, {:"$1", token, :_, :_}) do
-      [] ->
-        :ok
-
-      [[name] | _] ->
-        unless(name == nil,
-          do: :ets.insert(table, {name, token, ttl, DateTime.now!("Europe/Warsaw")})
-        )
-    end
-
-    {:reply, :ok, table}
-  end
-
-  def handle_cast({:remove_token, username, token}, table) do
-    [{_username, lookup_token, _ttl, _last_update} | _] = :ets.lookup(table, username)
-
-    if lookup_token == token do
-      :ets.delete(table, username)
-    end
-
-    {:noreply, table}
-  end
-
-  def handle_continue(:init_status, table), do: execute_token_refresh(table)
-
-  def handle_info(:refresh, table), do: execute_token_refresh(table)
-
-  defp execute_token_refresh(table) do
-    :ets.tab2list(table)
-    |> Task.async_stream(fn token -> refresh_token(table, token) end)
-    |> Enum.to_list()
-
-    Process.send_after(self(), :refresh, 60 * 60 * 1000)
-    {:noreply, table}
-  end
-
-  defp refresh_token(table, {username, token, ttl, last_update}) do
-    now = DateTime.now!("Europe/Warsaw")
-
-    if now <= Timex.shift(last_update, hours: ttl) do
-      case python(:helpers, :refresh_oauth, [token]) do
-        :ok ->
-          :ets.insert(table, {username, token, ttl, now})
-
-        {:error, __message} ->
-          :ets.delete(table, username)
-      end
-    else
-      :ets.delete(table, username)
-    end
-  end
+def start_link(args) do
+GenServer.start_link(__MODULE__, args, name: __MODULE__)
 end
+
+def init([table_name | opts]) do
+table = :ets.new(table_name, opts)
+{:ok, table, {:continue, :init_status}}
+end
+
+def handle_call({:add_token, username, token, ttl}, _from, table) do
+:ets.insert(table, {username, token, ttl, DateTime.now!("Europe/Warsaw")})
+{:reply, :ok, table}
+end
+
+def handle_call({:extend_lifetime, token}, _from, table) do
+case :ets.match(table, {:"$1", token, :"$2", :_}) do
+[] ->
+:ok
+
+[[name, ttl] | _] ->
+unless(name == nil,
+do: :ets.insert(table, {name, token, ttl, DateTime.now!("Europe/Warsaw")})
+)
+end
+
+{:reply, :ok, table}
+end
+
+def handle_call({:extend_lifetime, token, ttl}, _from, table) do
+case :ets.match(table, {:"$1", token, :_, :_}) do
+[] ->
+:ok
+
+[[name] | _] ->
+unless(name == nil,
+do: :ets.insert(table, {name, token, ttl, DateTime.now!("Europe/Warsaw")})
+)
+end
+
+{:reply, :ok, table}
+end
+
+def handle_cast({:remove_token, username, token}, table) do
+[{_username, lookup_token, _ttl, _last_update} | _] = :ets.lookup(table, username)
+
+if lookup_token == token do
+:ets.delete(table, username)
+end
+
+{:noreply, table}
+end
+
+def handle_continue(:init_status, table), do: execute_token_refresh(table)
+
+def handle_info(:refresh, table), do: execute_token_refresh(table)
+
+defp execute_token_refresh(table) do
+:ets.tab2list(table)
+|> Task.async_stream(fn token -> refresh_token(table, token) end)
+|> Enum.to_list()
+
+Process.send_after(self(), :refresh, 30 * 60 * 1000)
+{:noreply, table}
+end
+
+defp refresh_token(table, {username, token, ttl, last_update}) do
+now = DateTime.now!("Europe/Warsaw")
+
+if now <= Timex.shift(last_update, hours: ttl) do case python(:helpers, :refresh_oauth, [token]) do :ok ->
+  :ets.insert(table, {username, token, ttl, now})
+
+  {:error, __message} ->
+  :ets.delete(table, username)
+  end
+  else
+  :ets.delete(table, username)
+  end
+  end
+  end
