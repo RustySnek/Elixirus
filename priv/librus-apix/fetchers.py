f87e6306d63fd8177b5e6427f4b55cb17bc5fae0
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-22 12:14:14 +0200

    :snake: Return atomized Maps if safe

---
+++ priv/librus-apix/fetchers.py
@@ -0,0 +1,285 @@
+from erlport.erlterms import Atom
+from librus_apix.attendance import ParseError
+from librus_apix.completed_lessons import get_completed
+from librus_apix.exceptions import TokenError
+from librus_apix.grades import get_grades
+from librus_apix.student_information import get_student_information
+from librus_apix.notifications import get_notifications
+from librus_apix.client import AuthorizationError, Client
+from handle_classes import handle_completed_lessons
+from helpers import brew_elixir_list, create_client, brew_elixir_dict
+from erlport.erlterms import Atom
+from librus_apix.attendance import get_attendance, get_attendance_frequency
+from librus_apix.messages import (
+    get_received,
+    message_content,
+    get_max_page_number as get_max_page_messages,
+    recipient_groups,
+    get_recipients,
+    get_sent,
+    send_message as send_msg,
+)
+from librus_apix.homework import get_homework, homework_detail
+from librus_apix.schedule import get_schedule
+from librus_apix.announcements import get_announcements
+from librus_apix.completed_lessons import get_completed
+from librus_apix.student_information import get_student_information
+from handle_classes import *
+from datetime import datetime
+from librus_apix.exceptions import TokenError, ParseError
+
+from collections import Counter
+
+
+def sanitize_fetch(func, *args):
+    try:
+        return "ok", func(*args)
+    except TokenError as token_err:
+        return Atom("token_error".encode("utf-8")), str(token_err)
+    except ParseError as parse_err:
+        return Atom("error".encode("utf-8")), str(parse_err)
+    except AuthorizationError as err:
+        return Atom("error".encode("utf-8")), str(err)
+
+
+def fetch_student_data(token):
+    client: Client = create_client(token)
+    status, student_data = sanitize_fetch(get_student_information, client)
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_dict(
+            student_data.__dict__, safe=False
+        )
+    else:
+        return status, student_data
+
+
+def fetch_completed_lessons(token, date_from, date_to, page=0):
+    client: Client = create_client(token)
+    status, completed_lessons = sanitize_fetch(
+        get_completed, client, date_from, date_to, page
+    )
+
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_list(
+            handle_completed_lessons(completed_lessons), safe=False
+        )
+    else:
+        return status, completed_lessons
+
+
+def fetch_todays_completed_lessons(token):
+    client: Client = create_client(token)
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    return fetch_completed_lessons(client, today_date, today_date)
+
+
+def fetch_announcements(token):
+    client: Client = create_client(token)
+    status, announcements = sanitize_fetch(get_announcements, client)
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_list(
+            handle_announcements(announcements), safe=False
+        )
+    else:
+        return status, announcements
+
+
+def get_recipient_groups(token):
+    client: Client = create_client(token)
+    status, groups = sanitize_fetch(recipient_groups, client)
+    if status == "ok":
+        assert isinstance(groups, list)
+        return Atom(status.encode("utf-8")), brew_elixir_list(groups)
+    else:
+        return status, groups
+
+
+def get_group_recipients(token, group):
+    client: Client = create_client(token)
+    status, recipients = sanitize_fetch(get_recipients, client, group)
+    if status == "ok":
+        assert isinstance(recipients, dict)
+        return Atom(status.encode("utf-8")), brew_elixir_dict(recipients)
+    else:
+        return status, recipients
+
+
+def send_message(token, title, content, recipients):
+    client: Client = create_client(token)
+    recipients = list(map(lambda id: id.decode("utf-8"), recipients))
+    status, msg = sanitize_fetch(send_msg, client, title, content, recipients)
+    if status == "ok":
+        was_sent, msg = msg
+        if was_sent == False:
+            return Atom("send_error".encode("utf-8")), str(msg)
+        return Atom(status.encode("utf-8")), str(msg)
+    else:
+        return status, msg
+
+
+def fetch_sent_messages(token, page):
+    client: Client = create_client(token)
+    status, messages = sanitize_fetch(get_sent, client, int(page))
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_list(
+            handle_messages(messages), safe=False
+        )
+    else:
+        return status, messages
+
+
+def fetch_messages(token, page):
+    client: Client = create_client(token)
+    status, messages = sanitize_fetch(get_received, client, int(page))
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_list(
+            handle_messages(messages), safe=False
+        )
+    else:
+        return status, messages
+
+
+def fetch_all_messages(token):
+    client: Client = create_client(token)
+    try:
+        pages = get_max_page_messages(client)
+        messages = []
+        for page in range(0, pages + 1):
+            messages += get_received(client, page)
+    except TokenError as token_err:
+        return Atom("token_error".encode("utf-8")), str(token_err)
+    except ParseError as parse_err:
+        return Atom("error".encode("utf-8")), str(parse_err)
+    return Atom("ok".encode("utf-8")), brew_elixir_list(
+        handle_messages(messages), safe=False
+    )
+
+
+def fetch_message_content(token, id):
+    client: Client = create_client(token)
+    status, content = sanitize_fetch(message_content, client, id)
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_dict(
+            content.__dict__, safe=False
+        )
+    else:
+        return status, content
+
+
+def fetch_schedule(token, year, month):
+    client: Client = create_client(token)
+    status, schedule = sanitize_fetch(get_schedule, client, month, year, True)
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_dict(
+            handle_schedule(schedule), safe=False
+        )
+    else:
+        return status, schedule
+
+
+def fetch_grades(token, semester, opt):
+    client: Client = create_client(token)
+    status, data = sanitize_fetch(get_grades, client, opt)
+    if status == "ok":
+        subjects, semester_grades, descriptive = data
+        return Atom(status.encode("utf-8")), [
+            brew_elixir_dict(handle_grades(subjects[int(semester)]), safe=False),
+            brew_elixir_dict(handle_semester_grades(semester_grades)),
+        ]
+    else:
+        return status, data
+
+
+def fetch_all_grades(token, semester):
+    return fetch_grades(token, semester, "all")
+
+
+def fetch_new_grades(token, semester):
+    return fetch_grades(token, semester, "last_login")
+
+
+def fetch_week_grades(token, semester):
+    return fetch_grades(token, semester, "week")
+
+
+def fetch_homework(token, start, end):
+    client: Client = create_client(token)
+    status, homework = sanitize_fetch(get_homework, client, start, end)
+    if status == "ok":
+        return Atom(status.encode("utf-8")), brew_elixir_list(
+            handle_homework(homework), safe=False
+        )
+    else:
+        return status, homework
+
+
+def fetch_homework_details(token, id):
+    client: Client = create_client(token)
+    status, details = sanitize_fetch(homework_detail, client, id)
+    if status == "ok":
+        assert isinstance(details, dict)
+        return Atom(status.encode("utf-8")), brew_elixir_dict(details)
+    else:
+        return status, details
+
+
+def get_attendance_stats(attendance):
+    first, second = attendance
+    base_counter = Counter({"nb": 0, "sp": 0, "u": 0})
+    first = base_counter + Counter(a.symbol for a in first)
+    second = base_counter + Counter(a.symbol for a in second)
+    total = first + second
+    return brew_elixir_list([dict(first), dict(second), dict(total)])
+
+
+def fetch_attendance(token, semester, opt="all", get_stats=False):
+    client: Client = create_client(token)
+    status, attendance = sanitize_fetch(get_attendance, client, opt)
+    if status == "ok":
+        if get_stats:
+            return (
+                Atom(status.encode("utf-8")),
+                brew_elixir_list(
+                    handle_attendance(attendance[int(semester)]), safe=False
+                ),
+                get_attendance_stats(attendance),
+            )
+        else:
+            return Atom(status.encode("utf-8")), brew_elixir_list(
+                handle_attendance(attendance[int(semester)]), safe=False
+            )
+    else:
+        return status, attendance
+
+
+def fetch_all_attendance(token, semester, get_stats=False):
+    return fetch_attendance(token, semester, "all", get_stats)
+
+
+def fetch_week_attendance(token, semester, get_stats=False):
+    return fetch_attendance(token, semester, "week", get_stats)
+
+
+def fetch_new_attendance(token, semester, get_stats=False):
+    return fetch_attendance(token, semester, "last_login", get_stats)
+
+
+def fetch_attendance_frequency(token):
+    client: Client = create_client(token)
+    status, frequency = sanitize_fetch(get_attendance_frequency, client)
+    if status == "ok":
+        return Atom(status.encode("utf-8")), frequency
+    else:
+        return status, frequency
+
+
+def keep_token_alive(token):
+    client: Client = create_client(token)
+    status, notifications = sanitize_fetch(get_notifications, client)
+    if status == "ok":
+        client.refresh_oauth()
+        return Atom(status.encode("utf-8")), brew_elixir_list(
+            [n.__dict__ for n in notifications], safe=False
+        )
+    else:
+        return status, notifications
--- priv/librus-apix/overview.py
+++ priv/librus-apix/overview.py
@@ -1,23 +1,23 @@
 from erlport.erlterms import Atom
 from datetime import datetime
-from librus_apix.get_token import Token
-from librus_apix.grades import TokenError, get_grades, ParseError
+from librus_apix.exceptions import TokenError
+from librus_apix.grades import get_grades, ParseError
 from librus_apix.announcements import get_announcements
 from datetime import timedelta
 from librus_apix.attendance import get_attendance
 from librus_apix.homework import get_homework
-from librus_apix.messages import get_recieved
+from librus_apix.messages import get_received
 from librus_apix.schedule import get_schedule
 from librus_apix.student_information import get_student_information
 from librus_apix.timetable import get_timetable
 from handle_classes import *
-from helpers import create_token, extract_grades
+from helpers import create_client, extract_grades
 
 
 def handle_overview_grades(token, semester):
-    token = create_token(token)
+    client = create_client(token)
     try:
-        subjects, average_grades, descriptive = get_grades(token, "week")
+        subjects, average_grades, descriptive = get_grades(client, "week")
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
     except ParseError as parse_err:
@@ -28,7 +28,7 @@ def handle_overview_grades(token, semester):
 
 
 def handle_homework_overview(token, monday):
-    token = create_token(token)
+    token = create_client(token)
     try:
         this_week = (
             datetime.strptime(monday, "%Y-%m-%d") + timedelta(days=7)
@@ -42,9 +42,9 @@ def handle_homework_overview(token, monday):
 
 
 def handle_overview_student_info(token):
-    token = create_token(token)
+    client = create_client(token)
     try:
-        info = get_student_information(token).__dict__
+        info = get_student_information(client).__dict__
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
     except ParseError as parse_err:
@@ -53,9 +53,9 @@ def handle_overview_student_info(token):
 
 
 def handle_overview_messages(token):
-    token = create_token(token)
+    client = create_client(token)
     try:
-        messages = get_recieved(token, 0)
+        messages = get_received(client, 0)
         messages = filter(lambda x: x.unread == True, messages)
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
@@ -65,9 +65,9 @@ def handle_overview_messages(token):
 
 
 def handle_overview_announcements(token, amount):
-    token = create_token(token)
+    client = create_client(token)
     try:
-        announcements = get_announcements(token)
+        announcements = get_announcements(client)
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
     except ParseError as parse_err:
@@ -78,11 +78,11 @@ def handle_overview_announcements(token, amount):
 
 
 def handle_overview_timetable(token, monday):
-    token = create_token(token)
+    client = create_client(token)
     try:
         monday = "".join([chr(n) for n in monday])
         monday = datetime.strptime(monday, "%Y-%m-%d")
-        timetable = get_timetable(token, monday)
+        timetable = get_timetable(client, monday)
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
     except ParseError as parse_err:
@@ -91,9 +91,9 @@ def handle_overview_timetable(token, monday):
 
 
 def handle_overview_schedule(token, year, month):
-    token = create_token(token)
+    client = create_client(token)
     try:
-        schedule = get_schedule(token, month, year)
+        schedule = get_schedule(client, month, year)
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
     except ParseError as parse_err:
@@ -102,9 +102,9 @@ def handle_overview_schedule(token, year, month):
 
 
 def handle_overview_attendance(token, semester):
-    token = create_token(token)
+    client = create_client(token)
     try:
-        attendance = get_attendance(token, "week")
+        attendance = get_attendance(client, "week")
     except TokenError as token_err:
         return Atom("token_error".encode("utf-8")), str(token_err)
     except ParseError as parse_err:
