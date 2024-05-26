2ddc5cb9c56cc8777e7d49f0ea6c9f4992eb7de0
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-26 22:38:43 +0200

    :construction: W.I.P broken :poop:

--- lib/elixirus/token_worker.ex
+++ lib/elixirus/token_worker.ex
@@ -14,37 +14,45 @@ defmodule Elixirus.TokenWorker do
   def init(table) do
     Logger.warning("restarted token worker")
     # Start refresh process after a minute (avoids crashing genserver)
-    Process.send_after(self(), :refresh, 60_000)
+    Process.send_after(self(), :refresh, 1_000)
     {:ok, table}
   end
 
-  def handle_call({:add_token, username, token, ttl}, _from, table) do
-    :ets.insert(table, {username, token, ttl, DateTime.now!("Europe/Warsaw")})
+  def handle_call({:add_token, username, token, ttl, notification_token}, _from, table) do
+    :ets.insert(table, {username, token, ttl, notification_token, DateTime.now!("Europe/Warsaw")})
     {:reply, :ok, table}
   end
 
-  def handle_call({:extend_lifetime, token}, _from, table) do
+  def handle_call({:extend_lifetime, token, notification_token}, _from, table) do
     case :ets.match(table, {:"$1", token, :"$2", :_}) do
       [] ->
         :ok
 
       [[name, ttl] | _] ->
         unless(name == nil,
-          do: :ets.insert(table, {name, token, ttl, DateTime.now!("Europe/Warsaw")})
+          do:
+            :ets.insert(
+              table,
+              {name, token, ttl, notification_token, DateTime.now!("Europe/Warsaw")}
+            )
         )
     end
 
     {:reply, :ok, table}
   end
 
-  def handle_call({:extend_lifetime, token, ttl}, _from, table) do
+  def handle_call({:extend_lifetime, token, notification_token, ttl}, _from, table) do
     case :ets.match(table, {:"$1", token, :_, :_}) do
       [] ->
         :ok
 
       [[name] | _] ->
         unless(name == nil,
-          do: :ets.insert(table, {name, token, ttl, DateTime.now!("Europe/Warsaw")})
+          do:
+            :ets.insert(
+              table,
+              {name, token, ttl, notification_token, DateTime.now!("Europe/Warsaw")}
+            )
         )
     end
 
@@ -52,7 +60,8 @@ defmodule Elixirus.TokenWorker do
   end
 
   def handle_cast({:remove_token, username, token}, table) do
-    [{_username, lookup_token, _ttl, _last_update} | _] = :ets.lookup(table, username)
+    [{_username, lookup_token, _ttl, _notification_token, _last_update} | _] =
+      :ets.lookup(table, username)
 
     if lookup_token == token do
       :ets.delete(table, username)
@@ -65,46 +74,77 @@ defmodule Elixirus.TokenWorker do
 
   def handle_info(:refresh, table), do: execute_token_refresh(table)
 
+  def handle_cast({:push_notifications, notifications, token}, table) do
+    IO.inspect(notifications)
+
+    notifications
+    |> Enum.each(fn notification ->
+      dbg(notification)
+
+      notify =
+        Pigeon.FCM.Notification.new(
+          {:token, token},
+          %{notification.destination => "#{notification.amount}"}
+        )
+        |> Elixirus.FCM.push()
+        |> dbg
+
+      if notify.response != :success do
+        Logger.error("Sending notification Failed")
+        notify.error |> inspect() |> Logger.error()
+      end
+    end)
+    |> dbg
+
+    {:noreply, table}
+  end
+
   defp execute_token_refresh(table) do
     :ets.tab2list(table)
     |> Task.async_stream(fn token -> refresh_token(table, token) end, timeout: 120_000)
     |> Task.async_stream(
-      fn notification ->
-        # TODO push notifications
-        unless(notification == nil, do: :ok)
+      fn {_, {notifications, notification_token}} ->
+        dbg(notification_token)
+
+        if notifications != nil and notification_token not in [nil, ""] do
+          GenServer.cast(self(), {:push_notifications, notifications, notification_token})
+        end
       end,
       timeout: 60_000
     )
     |> Stream.run()
 
-    Process.send_after(self(), :refresh, 15 * 60 * 1000)
+    Process.send_after(self(), :refresh, 15 * 1000)
     {:noreply, table}
   end
 
-  defp refresh_token(table, {username, token, ttl, last_update}) do
+  defp refresh_token(table, {username, token, ttl, notification_token, last_update}) do
     now = DateTime.now!("Europe/Warsaw")
 
-    if DateTime.compare(now, Timex.shift(last_update, hours: ttl)) in [:lt, :eq] do
-      case python(:fetchers, :keep_token_alive, [token]) do
-        {:ok, notifications} ->
-          :ets.insert(table, {username, token, ttl, now})
-          notifications
+    notifications =
+      if DateTime.compare(now, Timex.shift(last_update, hours: ttl)) in [:lt, :eq] do
+        case python(:fetchers, :keep_token_alive, [token]) do
+          {:ok, notifications} ->
+            :ets.insert(table, {username, token, ttl, notification_token, now})
+            notifications
 
-        # do something with notifications
+          # do something with notifications
 
-        {:error, message} ->
-          Logger.error(message)
-          :ets.delete(table, username)
-          nil
+          {:error, message} ->
+            Logger.error(message)
+            :ets.delete(table, username)
+            nil
 
-        exception ->
-          Logger.error(exception |> Map.get(:error, "") |> to_string())
-          nil
+          exception ->
+            Logger.error(exception |> Map.get(:error, "") |> to_string())
+            nil
+        end
+      else
+        Logger.info("Removing expired token")
+        :ets.delete(table, username)
+        nil
       end
-    else
-      Logger.info("Removing expired token")
-      :ets.delete(table, username)
-      nil
-    end
+
+    {notifications, notification_token}
   end
 end
