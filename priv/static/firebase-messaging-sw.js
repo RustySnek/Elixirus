2e34ea48cbcffed09555a989c10adf5c88ea01f5
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-27 16:54:48 +0200

    :sparkles: onMessage callbaxck

--- priv/static/firebase-messaging-sw.js
+++ priv/static/firebase-messaging-sw.js
@@ -14,8 +14,15 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
+messaging.onMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
