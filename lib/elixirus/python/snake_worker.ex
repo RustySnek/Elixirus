af7e7f5439db30ca327f3ae22f42f211a7846ccb
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-05 19:09:48 +0200

    :zap: Remake python genserver

--- lib/elixirus/python/snake_worker.ex
+++ lib/elixirus/python/snake_worker.ex
@@ -19,56 +19,21 @@ defmodule Elixirus.Python.SnakeWorker do
         {:error, :rip_python}
 
       {:ok, pid} ->
-        {:ok, set_state(pid, :busy), {:continue, :init_data_types}}
+        {:ok, set_state(pid), {:continue, :init_data_types}}
     end
   end
 
   def handle_continue(:init_data_types, state) do
     :python.call(state.pid, :helpers, :setup_data_types, [])
-    {:noreply, set_state(state.pid, :ready)}
-  end
-
-  def handle_cast({:set_state, new_state}, state) do
-    {:noreply, set_state(state.pid, new_state)}
-  end
-
-  def handle_call(:kill_snake, _from, state) do
-    stop = :python.stop(state.pid)
-    {:noreply, stop, %{pid: state.pid, state: :dead, update: state.update}}
-  end
-
-  def handle_call({:run, module, func, args}, _from, state) do
-    GenServer.cast(self(), {:set_state, :busy})
-
-    response =
-      try do
-        data = :python.call(state.pid, module, func, args)
-        GenServer.cast(self(), {:set_state, :ready})
-        data
-      rescue
-        error ->
-          case error do
-            %ErlangError{original: {:python, exception, error, backtrace}} ->
-              Logger.error("#{exception}\n#{error}\nBacktrace: #{backtrace}")
-              :python.stop(state.pid)
-              Process.send_after(SnakeManager, {:sacrifice_snake, self()}, 200)
-              %{error: exception |> to_string}
-
-            exception ->
-              Logger.error(exception |> inspect)
-              %{error: "Unknown error occured."}
-          end
-      end
-
-    {:reply, response, state}
+    {:noreply, set_state(state.pid)}
   end
 
   def handle_call(:status, _from, state) do
-    {:reply, {state.state, state.update}, state}
+    {:reply, {state.pid, state.update}, state}
   end
 
-  defp set_state(pid, state) do
+  defp set_state(pid) do
     update = DateTime.now!("Europe/Warsaw")
-    %{pid: pid, state: state, update: update}
+    %{pid: pid, update: update}
   end
 end
--- lib/elixirus/python/snake_wrapper.ex
+++ lib/elixirus/python/snake_wrapper.ex
@@ -8,7 +8,52 @@ defmodule Elixirus.Python.SnakeWrapper do
   Call python GenServer
   """
 
-  def python(module, func, args, timeout \\ 120_000) do
-    GenServer.call(SnakeManager, {:run, module, func, args}, timeout)
+  require Logger
+
+  def _slay_python_worker(pid, pypid) do
+    :python.stop(pypid)
+    send(SnakeManager, {:sacrifice_snake, pid})
+  end
+
+  def python(module, func, args) do
+    # Prevents :python processes from not exiting
+    Process.flag(:trap_exit, true)
+
+    case GenServer.call(SnakeManager, :get_ready_snake, :infinity) do
+      {:error, _} ->
+        receive do
+          {:EXIT, _from, reason} -> Logger.warning("EXITED #{reason |> inspect}")
+        after
+          1500 ->
+            python(module, func, args)
+        end
+
+      {pid, pypid} ->
+        receive do
+          {:EXIT, _from, type} ->
+            _slay_python_worker(pid, pypid)
+            Logger.warning("EXITED #{type |> inspect}")
+        after
+          150 ->
+            try do
+              data = :python.call(pypid, module, func, args)
+              GenServer.call(SnakeManager, {:employ_snake, pid}, :infinity)
+              data
+            rescue
+              error ->
+                case error do
+                  %ErlangError{original: {:python, exception, error, backtrace}} ->
+                    Logger.error("#{exception}\n#{error}\nBacktrace: #{backtrace}")
+                    _slay_python_worker(pid, pypid)
+                    %{error: exception |> to_string}
+
+                  exception ->
+                    Logger.error(exception |> inspect)
+                    _slay_python_worker(pid, pypid)
+                    %{error: "Unknown error occured."}
+                end
+            end
+        end
+    end
   end
 end
