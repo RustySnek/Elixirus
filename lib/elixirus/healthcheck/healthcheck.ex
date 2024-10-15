2da31ddb8669cc4e0a3cc192ed774971a135adbf
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-10-15 18:17:10 +0200

    :skull: additional event triggering proxy (lets hope its lgtm)

--- lib/elixirus/healthcheck/healthcheck.ex
+++ lib/elixirus/healthcheck/healthcheck.ex
@@ -2,6 +2,6 @@ defmodule Elixirus.Healthcheck.Healthcheck do
   @moduledoc """
   wrapper for Healthcheck genserver
   """
-  
+
   def get_service_status(service), do: GenServer.call(service, {:get_status})
 end
