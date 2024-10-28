b47fdc8f905a0f131abdb10828fe5cd1a27b5b48
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-10-28 15:54:10 +0100

    :pencil: fix typo

--- lib/elixirus_web/helpers.ex
+++ lib/elixirus_web/helpers.ex
@@ -72,7 +72,7 @@ defmodule ElixirusWeb.Helpers do
   end
 
   def match_basic_errors(socket, {:error, :timeout}, _asyncs) do
-    status = Healthcheck.get_service_status(Elixirus.Healthcheck.LibrusConnection)
+    status = Healthcheck.get_service_status(Services.Healthcheck.LibrusConnection)
     proxy = System.get_env("USE_PROXY", "no")
     Logger.error("Librus connection on timing out: #{status} | proxy after refresh: #{proxy}")
 
